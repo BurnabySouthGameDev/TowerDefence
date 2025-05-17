d2f53aa2d2d5662e6930f80109a891dc7dfe0052
jayng9663 <jayng9663@gmail.com>
2025-05-16 19:02:04 -0700

    Remove enemy as soon as it reach the end of the path

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -14,8 +14,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if $".".get_parent() is Path3D and progress_ratio != 1:
 		$".".progress += speed/10
-	else:
-		get_tree().quit()
+		if progress_ratio >= 1.0:
+			# Later can add some thing like - 10 HP
+			var currency_label = get_tree().root.get_node("Main/GameUI/CurrencyDisplay/CurrencyLabel")
+			currency_label.subtract(50)
+			print("ememy reach the end of the path")
+			queue_free()
+	#else:
+	#	get_tree().quit()
 	
 	# Unused for now
 		
