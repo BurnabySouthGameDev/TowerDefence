599210d451112668e92997362676dca97af1beee
jayng9663 <jayng9663@gmail.com>
2025-05-16 19:06:31 -0700

    Small change

--- Scripts/turrets/basic_turret.gd
+++ Scripts/turrets/basic_turret.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
-@onready
-var radar : Radar = $"Radar"
+@onready var radar: Radar = $"Radar"
+@onready var reload_timer: Timer = $"ReloadTimer"
 
-@onready
-var reload_timer : Timer = $"ReloadTimer"
+func _ready() -> void:
+	radar.monitoring = false
 
 func _on_radar_new_target(target: Node3D) -> void:
+	if not radar.monitoring:
+		return
+
 	print("basic_turret - target: ", target)
 	if target == null:
 		reload_timer.stop()
@@ -17,12 +20,15 @@ func _on_radar_new_target(target: Node3D) -> void:
 		reload_timer.start()
 
 func fire() -> void:
-	assert(radar.current_target != null)
-
-	_fire_at(radar.current_target)
+	if radar.current_target != null:
+		_fire_at(radar.current_target)
 
 func _fire_at(target: Node3D) -> void:
-	var face := global_position.direction_to(target.global_position)
-	quaternion = Quaternion(Vector3.FORWARD, face)
+	var target_pos = target.global_position
+	target_pos.y = global_position.y  # Flatten Y to prevent pitch/roll
+	look_at(target_pos, Vector3.UP)
+
 	if randf() < (1.0 / 5.0):
+		var currency_label = get_tree().root.get_node("Main/GameUI/CurrencyDisplay/CurrencyLabel")
+		currency_label.add(5)
 		target.queue_free()
